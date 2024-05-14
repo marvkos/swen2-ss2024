f25926a5a590de42b89091afb7458e254c287d5c
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2024-05-14 10:50:01 +0200

    fix hibernate requierement error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module at.technikum.firstui {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
     opens at.technikum.firstui to javafx.fxml;
     opens at.technikum.firstui.view to javafx.fxml;
